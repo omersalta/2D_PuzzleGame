6057dda350018f118f14465040f28840b9354350
omersalta <omersalta@gmail.com>
2021-12-04 16:39:49 +0300

    Tile edited

--- Assets/Scripts/Game/Tile.cs
+++ Assets/Scripts/Game/Tile.cs
@@ -5,9 +5,14 @@ namespace Game {
     public class Tile : MonoBehaviour {
 
         private SpriteRenderer _renderer;
-        public Vector2 coordinate { get; private set;  }
+        public Vector2 coordinate { get; private set; }
         public Drop drop;
+        public Spawner spawner { get; private set; }
     
+        public void Initialize(int x, int y, Spawner mySpawner) {
+            spawner = mySpawner;
+            coordinate = new Vector2(x, y);
+        }
     }
 
 }
