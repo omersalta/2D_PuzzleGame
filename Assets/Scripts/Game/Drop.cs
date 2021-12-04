2fee702da86f468cb1acc4f7651cc286d46d743f
omersalta <omersalta@gmail.com>
2021-12-04 19:16:37 +0300

    drop script adited random color setter added

--- Assets/Scripts/Game/Drop.cs
+++ Assets/Scripts/Game/Drop.cs
@@ -1,11 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+using SO_Scripts.Managers;
 using UnityEngine;
 
-public class Drop : MonoBehaviour {
+namespace Game {
+
+    public class Drop : MonoBehaviour {
+        
+        public enum eColor {
+            Blue,
+            Green,
+            Red,
+            Yellow
+        }
+        
+        private SpriteRenderer _mySpriteRenderer;
+        public eColor color { get; private set; }
+
+        public void FirstInitialize(eColor color) {
+            _mySpriteRenderer = GetComponent<SpriteRenderer>();
+            this.color = color;
+            _mySpriteRenderer.sprite = MasterManager.dropSprites[(int) color];
+            
+        }
     
-    public bool mark { get; private set; }
-    
-    
-    
+    }
+
 }
