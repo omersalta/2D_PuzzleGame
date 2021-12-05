ad8eda0c9292f796f44d5c6005c80370138bf2a8
omersalta <omersalta@gmail.com>
2021-12-06 01:56:58 +0300

    Name Space added and little changes

--- Assets/Scripts/Game/Drop.cs
+++ Assets/Scripts/Game/Drop.cs
@@ -1,32 +1,43 @@
+using System;
 using SO_Scripts.Managers;
 using UnityEngine;
+using Utilities.RecycleGameObject;
 
 namespace Game {
 
-    public class Drop : MonoBehaviour {
-        
-        public enum eColor {
+    public class Drop : MonoBehaviour,IRecyle 
+    {
+
+        public enum dropColors {
             Blue,
             Green,
             Red,
             Yellow
         }
         
-        private SpriteRenderer _mySpriteRenderer;
-        public eColor color { get; private set; }
+        public dropColors color { get; private set; }
 
-        public void FirstInitialize(eColor color) {
-            _mySpriteRenderer = GetComponent<SpriteRenderer>();
+        public void FirstInitialize(dropColors color) {
             this.color = color;
-            _mySpriteRenderer.sprite = MasterManager.dropSprites[(int) color];
-            
+            Restart();
         }
         
-        public void MoveTo(Tile target) {
-            target.drop = this;
-            transform.localPosition = target.transform.localPosition;
+        public void Restart() {
+            SetSpriteRenderer();
         }
-    
+        
+        public void Shutdown() {
+            //TODO call explosion effect and add some score to player...
+        }
+        
+        public void SetTile(Tile target) {
+            target.drop = this;
+        }
+        
+        private void SetSpriteRenderer() {
+            GetComponent<SpriteRenderer>().sprite = MasterManager.dropSprites[(int) color];
+        }
+        
     }
 
 }
--- Assets/Scripts/Game/Spawner.cs
+++ Assets/Scripts/Game/Spawner.cs
@@ -2,70 +2,108 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Utilities;
-using Game;
-using SO_Scripts.Managers;
 using Utilities.RecycleGameObject;
 
-public class Spawner : MonoBehaviour {
+namespace Game {
 
-    public int columnNo { get; private set; }
-    private GameObject _dropPrefab;
-    public bool Activate;
-    private List<Tile> myColumnTiles;
+    public class Spawner : MonoBehaviour {
 
-    public int GetCurrentTileCount() {
-        return myColumnTiles.Count;
-    }
+        public int columnNo { get; private set; }
+        private GameObject _dropPrefab;
+        public bool Activate;
+        [SerializeField]
+        private List<Tile> myColumnTiles;
 
-    public void AddTile(Tile tile) {
-        myColumnTiles.Add(tile);
-        //after every adding ordering coordinate y values
-        myColumnTiles.OrderByDescending(x=>x.coordinate.y).ToList();
-    }
-    
-    public void Initialize(int myColumnNo , GameObject dropPrefab) {
-        _dropPrefab = dropPrefab;
-        columnNo = myColumnNo;
-        Activate = true;
-        myColumnTiles = new List<Tile>();
-    }
-    
-    public Drop CreateDrop() {
+        public int GetCurrentTileCount() { return myColumnTiles.Count; }
+
+        public void AddTile(Tile tile) {
         
-        GameObject dropGO;
-        dropGO = GameObjectUtil.Instantiate(_dropPrefab, transform.localPosition);
-        dropGO.transform.localPosition = transform.position;
-        Drop drop = dropGO.GetComponent<Drop>();
-        drop.FirstInitialize(GetRandomColor());
-        myColumnTiles[0].drop = drop;
-        FallDrops();
-        return drop.GetComponent<Drop>();
+            if (myColumnTiles.Any(t => t.coordinate.y == tile.coordinate.y)) {
+                Debug.LogWarning("there is already same level tile");
+            }
         
+            myColumnTiles.Insert(0, tile);
+
+            //after every adding ordering coordinate y values (y = 0 is the last one)
+            myColumnTiles = myColumnTiles.OrderByDescending(tile => tile.coordinate.y).ToList();
+
+        }
+    
+        public void OnClick_AvtisionButton() {
+            //TODO add clickable activision spawner and show to user with visual something (text,sprite...)
+        }
+
+        public void Initialize(int myColumnNo, GameObject dropPrefab) {
+            _dropPrefab = dropPrefab;
+            columnNo = myColumnNo;
+            Activate = true;
+            myColumnTiles = new List<Tile>();
+        }
+    
+        public void Initialize(int myColumnNo, GameObject dropPrefab, bool activationState) {
+            _dropPrefab = dropPrefab;
+            columnNo = myColumnNo;
+            Activate = activationState;
+            myColumnTiles = new List<Tile>();
+        }
+
+        public void CreateDrop() {
+        
+            if (!Activate) {
+                return;
+            }
+
+            _CreateDrop();
+        }
+    
+        private void _CreateDrop() {
+        
+            GameObject dropGO;
+            dropGO = GameObjectUtil.Instantiate(_dropPrefab, transform.localPosition);
+            dropGO.transform.localPosition = transform.position;
+            Drop drop = dropGO.GetComponent<Drop>();
+            drop.FirstInitialize(GetRandomColor());
+            Tile EmptyTile = GetUndermostEmptyTile();
+            EmptyTile.drop = drop;
+            drop.SetTile(EmptyTile);
+        
+        }
+
+        private Tile GetUndermostEmptyTile() {
+            //its return undermost empty tile
+            for (int i = myColumnTiles.Count - 1; -1 < i; i--) {
+                if (myColumnTiles[i].drop == null) {
+                    return myColumnTiles[i];
+                }
+            }
+
+            Debug.LogWarning("there is no empty tile");
+            return null;
+        }
+
+        public void FallDrops() {
+            int currentIndex = -1;
+
+            foreach (Tile tile in myColumnTiles) {
+                currentIndex++;
+                int filledCount = 0;
+                int emptyCount = 0;
+
+                for (int i = currentIndex; i < myColumnTiles.Count - 1; i++) {
+                    if (myColumnTiles[i + 1].drop == null) { emptyCount++; } else { filledCount++; }
+                }
+
+                tile.drop?.SetTile(myColumnTiles[currentIndex + emptyCount]);
+            
+            }
+        }
+    
+        private Drop.dropColors GetRandomColor() {
+            Drop.dropColors color;
+            color = (Drop.dropColors) typeof(Drop.dropColors).GetRandomEnumValue();
+            return color;
+        }
+
     }
 
-     public void FallDrops() {
-         int currentIndex = -1;
-         foreach (Tile tile in myColumnTiles) {
-             currentIndex++;
-             int filledCount = 0;
-             int emptyCount = 0;
-             for (int i = currentIndex; i < myColumnTiles.Count-1; i++) {
-                 if (myColumnTiles[i + 1].drop == null) {
-                     emptyCount++;
-                 } else {
-                     filledCount++;
-                 }
-             }
-             
-             tile.drop?.MoveTo(myColumnTiles[currentIndex + emptyCount]);
-             //MasterManager.boardManager.GetTile()
-         }
-     }
-    
-    
-    private Drop.eColor GetRandomColor() {
-        Drop.eColor color;
-        color = (Drop.eColor) typeof(Drop.eColor).GetRandomEnumValue();
-        return color;
-    }
 }
