a8ba2b53eed8306d632413529f5d1ab10b419692
omersalta <omersalta@gmail.com>
2021-12-07 02:25:23 +0300

    Game Manager main loop and  tween funcs are organized

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,3 +1,5 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Game {
@@ -10,10 +12,14 @@ namespace Game {
             ANIMATION,
             GAME_OVER,
         };
-
-    
-        private int animationCounter;
+        
+        //private int animationCounter;
         private State _currentState;
+        private Sequence _sequence;
+
+        private void Awake() {
+            DOTween.Init();
+        }
 
         private void Update() {
         
@@ -27,35 +33,67 @@ namespace Game {
                 //////////////////////////////////////////
                 //////////////////////////////////////////
                 case State.CHOSE_MOVE:
-
+                    //TODO do if gonna do extra something when choosing move 
                     break;
                 //////////////////////////////////////////
                 //////////////////////////////////////////
                 case State.ANIMATION:
-
+                    //TODO do if gonna do extra something when Tweening
                     break;
                 //////////////////////////////////////////
                 //////////////////////////////////////////
                 case State.GAME_OVER:
-                
+                    //TODO show game over panel and make recycle the game to play again
                     break;
             }
         
         }
 
+        private void StartTween() {
+            _currentState = State.ANIMATION;
+            _sequence = DOTween.Sequence();
+            //_sequence.onKill(OnStopTween);
+        }
+        
+        private void Tweening(Drop drop, Tile targetTile) {
+            _sequence.Join(drop.transform.DOMove(targetTile.transform.position, 1f));
+        }
+        
+        private void OnStopTween() {
+           Debug.Log("cheking explosion and calling another tweening after...");
+           //_sequence.Complete();
+           CheckExplosion();
+           _currentState = State.CHOSE_MOVE;
+        }
+
         public void Move(Tile firtsTile, Tile secondTile) {
+
+            if (_currentState != State.CHOSE_MOVE) {
+                return;
+            }
+            
             Debug.Log("move on "+ firtsTile +", "+secondTile);
+            StartTween();
+            Tweening(firtsTile.drop,secondTile);
+            Tweening(secondTile.drop,firtsTile);
+            SwitchTile(firtsTile, secondTile);
             //TODO start sequance and after check explosion
+
         }
 
-        public static void OnAnimationStart() {
-        
+        private void SwitchTile(Tile firtsTile, Tile secondTile) {
+            firtsTile.SwitchDrops(secondTile);
         }
+
+        private void CheckExplosion() {
+            //TODO if there is a explosion after move so; explode these drops and call CreateDrops() 
     
-        public static void OnAnimationFinish() {
-        
         }
 
+        private void CreateDrops() {
+            //TODO get spawners and how many create with a generic pair like KeyValuePair 
+        }
+        
     }
 
 }
